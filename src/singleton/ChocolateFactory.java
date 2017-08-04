783cc7010c7ae5b2dabe77214f3217771d94034a
superGG <459714770@qq.com>
2017-08-04 19:07:34 +0800

    Singleton Pattern

---
+++ src/singleton/ChocolateFactory.java
@@ -0,0 +1,46 @@
+package singleton;
+
+/**
+ * 单例模式 模拟巧克力工厂 在只有一个锅的情况下 加热 巧克力
+ * 
+ * @author Kellan 2017年8月4日
+ */
+public class ChocolateFactory {
+
+	private static ChocolateFactory uniqueInstance = null;
+
+	private boolean empty;
+	private boolean boiled;
+
+	private ChocolateFactory() {
+		empty = true;
+		boiled = false;
+	}
+
+	public synchronized static ChocolateFactory getInstance() {
+		if (uniqueInstance == null) {
+			uniqueInstance = new ChocolateFactory();
+		}
+		return uniqueInstance;
+	}
+
+	public void fill() {
+		if (empty) {
+			empty = false;
+			boiled = false;
+		}
+	}
+
+	public void boil() {
+		if (!empty && !boiled) {
+			boiled = true;
+		}
+	}
+
+	public void drain() {
+		if (!empty && boiled) {
+			empty = false;
+			boiled = false;
+		}
+	}
+}
