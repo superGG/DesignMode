4e8cde56fde7b25ad56bb6bf43b961371c8e4ff4
superGG <459714770@qq.com>
2017-08-04 16:17:13 +0800

    Observer Mode

--- src/observer/InternetWeather.java
+++ src/observer/InternetWeather.java
@@ -1,11 +1,17 @@
 package observer;
 
+/**
+ * 观察者模式的模拟实现
+ * 模拟： 天气检测站为被观察者，当天气信息发生改变时，统一通知所有已经注册的观察者。
+ * @author Kellan
+ * 2017年8月4日
+ */
 public class InternetWeather {
 
 	public static void main(String[] args) {
-		CurrentConditions currentConditions = new CurrentConditions();
-		TomorrowConditions tomorrowConditions = new TomorrowConditions();
-		WeatherData weatherData = new WeatherData();
+		CurrentConditions currentConditions = new CurrentConditions(); //观察者1
+		TomorrowConditions tomorrowConditions = new TomorrowConditions(); //观察者2
+		WeatherData weatherData = new WeatherData(); //被观察
 		
 		weatherData.registerObserver(currentConditions);
 		weatherData.registerObserver(tomorrowConditions);
